cc9e45de744f73194ff080e8a5b1d929b8f43f4a
KrishnajithUS <krishnajithus673@gmail.com>
2023-05-06 19:21:58 +0530

    gunicorn added

--- src/EmailSender/settings.py
+++ src/EmailSender/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'pranavhilarikanz666@gmail.com'
+EMAIL_HOST_PASSWORD = 'liblffhmrckggxwi'
